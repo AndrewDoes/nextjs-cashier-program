3f40aa8a5cb3d0d5e4c7240de399aa9f5c9450a9
Andrew Sutiono <sutionoandrew@gmail.com>
2025-08-03 22:09:51 +0700

    setup projects

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+// src/lib/firebase.js
+
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration from the .env.local file
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+// To prevent re-initialization on hot reloads, check if an app is already initialized.
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Get a reference to the Firestore database service
+const db = getFirestore(app);
+
+export { db };

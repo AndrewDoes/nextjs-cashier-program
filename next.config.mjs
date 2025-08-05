e86b619fe2095bf7e4ce2dc3af898398f4b7aaf0
Andrew Sutiono <sutionoandrew@gmail.com>
2025-08-06 02:52:49 +0700

    changed directory straight to the customer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Add the redirects function here
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/customer',
+        permanent: true, // This tells browsers it's a permanent move (HTTP 308)
+      },
+    ]
+  },
+};
 
 export default nextConfig;
